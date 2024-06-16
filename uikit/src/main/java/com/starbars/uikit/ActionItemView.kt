9474e55e8c32a34fa24d1c065181ad7761531bb8
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-16 15:47:38 +0300

    implements ActionItemView add resources

--- uikit/src/main/java/com/starbars/uikit/ActionItemView.kt
+++ uikit/src/main/java/com/starbars/uikit/ActionItemView.kt
@@ -2,6 +2,7 @@ package com.starbars.uikit
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -14,8 +15,53 @@ class ActionItemView @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0,
     @StyleRes defStyleRes: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
-    private lateinit var textView: TextView
-    private lateinit var imageView: ImageView
-    private lateinit var body: LinearLayout
+    private lateinit var titleTv: TextView
+    private lateinit var endTv: TextView
+    private lateinit var iconIv: ImageView
 
+    init {
+        initViews()
+        applyAttributes(context, attrs, defStyleAttr, defStyleRes)
+    }
+
+    private fun initViews() {
+        LayoutInflater.from(context).inflate(R.layout.action_item_view_layout, this, true)
+        titleTv = findViewById(R.id.tv_action_title)
+        iconIv = findViewById(R.id.iv_action_view_icon)
+        endTv = findViewById(R.id.tv_end_text)
+    }
+
+    private fun applyAttributes(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.ActionItemView,
+            defStyleAttr,
+            defStyleRes
+        ).apply {
+            try {
+                iconIv.setImageDrawable(getDrawable(R.styleable.ActionItemView_iconResource))
+                titleTv.text = getString(R.styleable.ActionItemView_itemTitleText) ?: ""
+                endTv.text = getString(R.styleable.ActionItemView_endText) ?: ""
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    fun setTitle(newTitle: String) {
+        titleTv.text = newTitle
+    }
+
+    fun setEndText(newText: String) {
+        endTv.text = newText
+    }
+
+    fun setIconDrawable(iconId: Int) {
+        iconIv.setImageResource(iconId)
+    }
 }
