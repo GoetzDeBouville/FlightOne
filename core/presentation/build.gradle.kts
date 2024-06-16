664d3170059dc61a30c10fdf8dc273b5e19528da
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-16 16:07:15 +0300

    plug in libs

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -39,4 +39,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
