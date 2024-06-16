e26c8a5ae5544c85055d1f5177f5c5974008a891
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-16 18:09:44 +0300

    setup statusbar

--- app/src/main/java/com/starbars/flightone/MainActivity.kt
+++ app/src/main/java/com/starbars/flightone/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.starbars.flightone
 
+import android.content.pm.ActivityInfo
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,20 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
-
-        binding.let {
-            ViewCompat.setOnApplyWindowInsetsListener(it.main) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
-        }
-
+        setPortraitOrientation()
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragment_container) as NavHostFragment
 
         binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
+    }
+
+    private fun setPortraitOrientation() {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
